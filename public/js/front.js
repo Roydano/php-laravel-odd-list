0f491820f792ef7d8c51319e7e9770397fad6a82
Roydano <roy.dano90@gmail.com>
2021-09-20 20:19:12 +0200

    add collegamento dettaglio al post

--- public/js/front.js
+++ public/js/front.js
@@ -2303,6 +2303,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: 'DettagliPost',
   data: function data() {
